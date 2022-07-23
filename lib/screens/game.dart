2f9dfcf63f48646f29cff43fe6b47c658b0560c3
XtarNodde <onyekaejimns@gmail.com>
2022-07-23 14:09:22 +0100

    second commit

--- lib/screens/game.dart
+++ lib/screens/game.dart
@@ -21,165 +21,18 @@ int index=0;
       body: Column(
         children: [
           Expanded(
-            child: PageView.builder(controller: controller,onPageChanged:(ind) {
+            //this is the selfdefined method of creating  pages in pageview
+            // child:PageView(children: [Center(child: Text("ada")),Center(child: Text("obi"))]) ,
+            // this is the method of auto generating or predefined pages using pageview
+            child: PageView.builder(
+              controller: controller,onPageChanged:(ind) {
 
               setState((){this.index=ind;});
             },
               itemCount: 2,
               itemBuilder: (context, index) {
                 this.index=index;
-                return Center(
-                    child: Column(
-                  children: [
-                    SizedBox(
-                      height: 10,
-                    ),
-                    SizedBox(
-                      child: Material(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                        elevation: 20,
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            child: Image.asset(
-                              "asset/imageguesser.jpg",
-                              fit: BoxFit.contain,
-                            )),
-                      ),
-                    ),
-                    Flexible(
-                        child: SizedBox(
-                      height: 500,
-                      width: 500,
-                      child: Column(children: [
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "What Is My Name",
-                              style: GoogleFonts.aldrich(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              height: 50,
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border:
-                                      Border.all(color: Colors.black, width: 2),
-                                  color: Colors.purple),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "a. $sugestiona",
-                                      textAlign: TextAlign.left,
-                                      style: GoogleFonts.aldrich(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Container(
-                              height: 50,
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border:
-                                      Border.all(color: Colors.black, width: 2),
-                                  color: Colors.purple),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "b. $sugestionb",
-                                      textAlign: TextAlign.left,
-                                      style: GoogleFonts.aldrich(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 5,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Container(
-                              height: 50,
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border:
-                                      Border.all(color: Colors.black, width: 2),
-                                  color: Colors.purple),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "c. $sugestionc",
-                                      textAlign: TextAlign.left,
-                                      style: GoogleFonts.aldrich(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Container(
-                              height: 50,
-                              width: 200,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border:
-                                      Border.all(color: Colors.black, width: 2),
-                                  color: Colors.purple),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      "d. $sugestiond",
-                                      textAlign: TextAlign.left,
-                                      style: GoogleFonts.aldrich(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
-                      ]),
-                    ))
-                  ],
-                ));
+                return GameScreen(sugestiona: sugestiona, sugestionb: sugestionb, sugestionc: sugestionc, sugestiond: sugestiond,image: "asset/imageguesser.jpg",correct_answer: "ada",);
               },
             ),
           ),
@@ -223,5 +76,177 @@ int index=0;
         ],
       ),
     );
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({
+    Key? key,
+    required this.sugestiona,
+    required this.sugestionb,
+    required this.sugestionc,
+    required this.sugestiond, required this.correct_answer,required this.image,
+  }) : super(key: key);
+
+  final String? sugestiona;
+  final String? sugestionb;
+  final String? sugestionc;
+  final String? sugestiond;
+  final String? correct_answer;
+  final String? image;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        SizedBox(
+          child: Material(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            elevation: 20,
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Image.asset(
+                  image!,
+                  fit: BoxFit.contain,
+                )),
+          ),
+        ),
+        Flexible(
+            child: SizedBox(
+          height: 500,
+          width: 500,
+          child: Column(children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "What Is My Name",
+                  style: GoogleFonts.aldrich(
+                      fontSize: 30, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border:
+                          Border.all(color: Colors.black, width: 2),
+                      color: Colors.purple),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          "a. $sugestiona",
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.aldrich(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border:
+                          Border.all(color: Colors.black, width: 2),
+                      color: Colors.purple),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          "b. $sugestionb",
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.aldrich(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border:
+                          Border.all(color: Colors.black, width: 2),
+                      color: Colors.purple),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          "c. $sugestionc",
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.aldrich(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border:
+                          Border.all(color: Colors.black, width: 2),
+                      color: Colors.purple),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Text(
+                          "d. $sugestiond",
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.aldrich(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ]),
+        ))
+      ],
+    ));
   }
 }
